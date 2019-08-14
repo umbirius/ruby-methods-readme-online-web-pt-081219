0d97f805f4e915c2317e5ae61019a5d9ea9309b4
loading-actor-9896 <upalazzo93@gmail.com>
2019-08-14 18:29:00 +0000

    Automatically backed up by Learn

--- greeting.rb
+++ greeting.rb
@@ -1,1 +1,13 @@
-def 
+def say_hello_world_ten_times
+  phrase = "Hello World!"
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+end
